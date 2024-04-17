6d32fdba335ee944274632742fe9468f6a426dc7
OdhiamboMoses <odhiambomoses08@gmail.com>
2024-04-17 18:34:34 +0300

    image slider

--- main.js
+++ main.js
@@ -4,3 +4,28 @@ myDashHeading.innerText = "ICT Student Dashboard";
 let myMainContHeading = document.querySelector("h3");
 myMainContHeading.innerText = "Transforming Urban Poverty into Urban promise";
 
+// Get all images
+
+const images = document.querySelectorAll('.image-slider img');
+
+let currentImageIndex = 0;
+
+function showNextImage() {
+
+// Hide the current image
+
+images[currentImageIndex].style.display = 'none';
+
+// Move to the next image
+
+currentImageIndex = (currentImageIndex + 1) % images.length;
+
+// Display the next image
+
+images[currentImageIndex].style.display = 'block';
+
+}
+
+// Show the next image every 5 seconds
+
+setInterval(showNextImage, 5000);
