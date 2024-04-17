b43f83cd9113ec599bb9c02470f5e38532d287df
OdhiamboMoses <odhiambomoses08@gmail.com>
2024-04-17 10:23:35 +0300

    Home tab content

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let myDashHeading = document.querySelector("h2");
 myDashHeading.innerText = "ICT Student Dashboard";
 
 let myMainContHeading = document.querySelector("h3");
-myMainContHeading.innerText = "Main Content";
+myMainContHeading.innerText = "Transforming Urban Poverty into Urban promise";
 
