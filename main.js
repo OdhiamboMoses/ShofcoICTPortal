28b70cdb49936ad07b0bc1bfde92291acf752166
OdhiamboMoses <odhiambomoses08@gmail.com>
2024-04-17 09:35:36 +0300

    Pages setup

---
+++ main.js
@@ -0,0 +1,6 @@
+let myDashHeading = document.querySelector("h2");
+myDashHeading.innerText = "ICT Student Dashboard";
+
+let myMainContHeading = document.querySelector("h3");
+myMainContHeading.innerText = "Main Content";
+
